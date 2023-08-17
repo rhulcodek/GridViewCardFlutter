38f20353f475fa35780260d47c260a93ea6ff8a0
Rahul_Choudhari <rahulchoudhari@whizsoftwares.com>
2023-08-17 11:50:13 +0000

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,83 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    debugShowMaterialGrid: false,
+    home: MyApp(),
+  ));
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  List<String> images = [
+    "https://d3nn873nee648n.cloudfront.net/900x600/100117/300-ZM1027221.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/3848/20-BG117636.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/17799/220-LA788644.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/17289/220-SQ761793.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/100117/300-ZM1027221.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/3848/20-BG117636.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/17799/220-LA788644.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/17289/220-SQ761793.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/100117/300-ZM1027221.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/3848/20-BG117636.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/17799/220-LA788644.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/17289/220-SQ761793.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/100117/300-ZM1027221.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/3848/20-BG117636.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/17799/220-LA788644.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/17289/220-SQ761793.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/100117/300-ZM1027221.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/3848/20-BG117636.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/17799/220-LA788644.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/17289/220-SQ761793.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/100117/300-ZM1027221.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/3848/20-BG117636.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/17799/220-LA788644.jpg",
+    "https://d3nn873nee648n.cloudfront.net/900x600/17289/220-SQ761793.jpg",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter GridView"),
+          backgroundColor: Colors.red,
+        ),
+        body: Container(
+          padding: EdgeInsets.all(10.0),
+          child: GridView.builder(
+            itemCount: images.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              crossAxisSpacing: 20.0,
+              mainAxisSpacing: 20.0,
+            ),
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                elevation: 10.0,
+                color: Color(0xff0c0c0c),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Column(
+                  children: [
+                    Image.network(images[index]),
+                    Padding(
+                      padding: EdgeInsets.all(30.0),
+                      child: Text(
+                        "Nature is not a place to visit, it is home",
+                        style: TextStyle(
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18.0,
+                            color: Color(0xffe8efe8)),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
